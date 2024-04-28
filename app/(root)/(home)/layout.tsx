f433c9d507b78e596577232aa74e5ebedb79f109
kirdua <kirdua@gmail.com>
2024-04-28 12:25:19 -0500

    metadata feature to layouts

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,12 @@
 import { ReactNode } from 'react'
+import { Metadata } from 'next';
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
+
+export const metadata: Metadata = {
+  title: 'YOOM',
+  description: 'A workspace for your team, powered by Stream Chat and Clerk.',
+};
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
