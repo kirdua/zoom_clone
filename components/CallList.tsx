0e3038b5bcb6b9a29da8e0d54760afeb1e9d0942
kirdua <kirdua@gmail.com>
2024-04-30 10:32:17 -0500

    add callList to recordings component

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -7,9 +7,12 @@ import { useGetCalls } from '@/hooks/useGetCalls'
 import MeetingCard from './MeetingCard'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import { useToast } from './ui/use-toast'
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
   const router = useRouter()
+  const { toast } = useToast()
+
   const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls()
   const [recordings, setRecordings] = useState<CallRecording[]>([])
 
@@ -41,15 +44,19 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
 
   useEffect(() => {
     const fetchRecordings = async () => {
-      const callData = await Promise.all(
-        callRecordings?.map((meeting) => meeting.queryRecordings()) ?? []
-      )
+      try {
+        const callData = await Promise.all(
+          callRecordings?.map((meeting) => meeting.queryRecordings()) ?? []
+        )
 
-      const recordings = callData
-        .filter((call) => call.recordings.length > 0)
-        .flatMap((call) => call.recordings)
+        const recordings = callData
+          .filter((call) => call.recordings.length > 0)
+          .flatMap((call) => call.recordings)
 
-      setRecordings(recordings)
+        setRecordings(recordings)
+      } catch (error) {
+        toast({ title: 'Please try again later' })
+      }
     }
 
     if (type === 'recordings') {
